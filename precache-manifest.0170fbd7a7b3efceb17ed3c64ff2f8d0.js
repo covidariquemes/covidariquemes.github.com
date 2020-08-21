0db75b197d999e38fa235bbd8d5725c402c46b38
covidariquemes <lucascarinhanha4@gmail.com>
2020-08-21 05:49:25 -0400

    Updates

---
+++ precache-manifest.0170fbd7a7b3efceb17ed3c64ff2f8d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6689d15640cc0febaec35a5dd3ef09ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5baef35f77bb07a5861d",
+    "url": "/static/css/main.75541f60.chunk.css"
+  },
+  {
+    "revision": "ef9793720e381d7d5497",
+    "url": "/static/js/2.af23b3ac.chunk.js"
+  },
+  {
+    "revision": "4db0b4b7fd2c7a0aaf4845dcd6ed7f11",
+    "url": "/static/js/2.af23b3ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5baef35f77bb07a5861d",
+    "url": "/static/js/main.d99b8d2a.chunk.js"
+  },
+  {
+    "revision": "4df097abbea25f86dac0",
+    "url": "/static/js/runtime-main.f10a8730.js"
+  }
+]);
